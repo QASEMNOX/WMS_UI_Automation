2dc932cf68cf65de949ffe5b219a1676123e2109
chaitra <chai.nir@gmail.com>
2026-02-26 08:13:13 +0530

    Updated WMS input data

--- tests/DataSetup/Products/Tax/Tax.spec.js
+++ tests/DataSetup/Products/Tax/Tax.spec.js
@@ -8,18 +8,18 @@ const TaxData = require('../../../../test-data/Products/Tax/tax.json');
 
 test.describe(' Create Multiple Tax', () => {
 
-    let poManager;
-    let page;
+  let poManager;
+  let page;
 
-    const username = 'semnox';
-    const password = 'semnoX!1';
-    const site = 'ORIONSITEA';
- 
+  const username = 'semnox';
+  const password = 'semnoX!1';
+  const site = 'ORIONSITEA';
 
-    test.beforeAll(async ({ browser }) => {
-        // launch new page only once
-         page = await browser.newPage();
-       poManager = new POManager(page);
+
+  test.beforeAll(async ({ browser }) => {
+    // launch new page only once
+    page = await browser.newPage();
+    poManager = new POManager(page);
     const loginPage = poManager.getLoginPage();
     await loginPage.goTo(testData.url);
     await loginPage.waitForPageToBeReady();
@@ -31,35 +31,35 @@ test.describe(' Create Multiple Tax', () => {
     await loginPage.waitForPageToBeReady();
     const homePage = poManager.getHomePage();
     await homePage.SelectSiteDropdown(testData.SiteSelection.SiteName);
-   /* await loginPage.waitForPageToBeReady();
-    await homePage.MenueSelection();
-    await loginPage.waitForPageToBeReady();
-    */
-    });
-
-for (const data of TaxData) {
- 
-  test(`Taxset for ${data.tax_Name}`, async ({  }) => {
-
-    
-const Tax = poManager.getTaxPage();
-await Tax.selectTaxMenu();
-await Tax.CreateTax(data.tax_Name, data.taxPercentage);
-
-//await page.getByText('IsUpsell').click();
-
-/*await expect(
-  page.locator('snack-bar-container')
-).toContainText('Record has been saved successfully');
-*/
-
-await Tax.closeTax_and_gotoHome();
-
+    /* await loginPage.waitForPageToBeReady();
+     await homePage.MenueSelection();
+     await loginPage.waitForPageToBeReady();
+     */
   });
-}
-test.afterAll(async () => {
-        await page.close();
+
+  for (const data of TaxData) {
+
+    test(`Taxset for ${data.tax_Name}`, async ({ }) => {
+
+
+      const Tax = poManager.getTaxPage();
+      await Tax.selectTaxMenu();
+      await Tax.CreateTax(data.tax_Name, data.taxPercentage);
+
+      //await page.getByText('IsUpsell').click();
+
+      /*await expect(
+        page.locator('snack-bar-container')
+      ).toContainText('Record has been saved successfully');
+      */
+
+      await Tax.closeTax_and_gotoHome();
+
     });
+  }
+  test.afterAll(async () => {
+    await page.close();
+  });
 
 });
 
